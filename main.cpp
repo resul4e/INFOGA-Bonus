369ee2d5b3cb860b8251ce628d6d17d8ae3877b8
Resul Çelik <resul_celik@hotmail.com>
2020-12-22 22:27:47 +0100

    Added sfml and glm to the cmake.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
+#include <SFML/Graphics.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
 #include <iostream>
 
-int main(int argc, char** argv)
+int main()
 {
-    std::cout << "Hello World!\n";
+    glm::vec2 point(10,20);
+    std::cout << to_string(point) << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
     return 0;
 }
